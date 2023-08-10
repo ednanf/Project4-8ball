7d089fc92b3c118a5d153fbbf7ec232c4af232d1
Ednan Rogério Frizzera Filho <ednanrff@gmail.com>
2023-08-10 12:13:57 -0300

    Project complete

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+// MARK: -Properties
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
+// MARK: -IBOutlets
+    @IBOutlet var imageView: UIImageView!
+    
+// MARK: -Main functions
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+// MARK: -IBActions
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        imageView.image = UIImage(named: ballArray.randomElement()!)
+    }
+    
+// MARK: -Functions
 
 }
-
